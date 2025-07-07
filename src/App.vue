82315037c3b70faf911620e69e3ca3ca31c0d4cf
muhnashir <muhammadnashir9@gmail.com>
2025-07-07 20:34:27 +0700

    add footer

--- src/App.vue
+++ src/App.vue
@@ -187,4 +187,15 @@ const generatePDF = () => {
       </div>
     </div>
   </div>
+  <footer class="text-center py-4 text-gray-500 text-sm">
+    <div class="flex justify-center items-center gap-2">
+      <span>From us, with love</span>
+      <a href="https://www.instagram.com/opetra_id" target="_blank" rel="noopener noreferrer" class="hover:text-pink-500">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 fill-current" viewBox="0 0 24 24">
+          <path d="M7.75 2A5.75 5.75 0 0 0 2 7.75v8.5A5.75 5.75 0 0 0 7.75 22h8.5A5.75 5.75 0 0 0 22 16.25v-8.5A5.75 5.75 0 0 0 16.25 2h-8.5Zm0 1.5h8.5A4.25 4.25 0 0 1 20.5 7.75v8.5a4.25 4.25 0 0 1-4.25 4.25h-8.5A4.25 4.25 0 0 1 3.5 16.25v-8.5A4.25 4.25 0 0 1 7.75 3.5Zm9.75 2a.75.75 0 1 0 0 1.5a.75.75 0 0 0 0-1.5ZM12 7a5 5 0 1 0 0 10a5 5 0 0 0 0-10Zm0 1.5a3.5 3.5 0 1 1 0 7a3.5 3.5 0 0 1 0-7Z"/>
+        </svg>
+      </a>
+    </div>
+  </footer>
+
 </template>
